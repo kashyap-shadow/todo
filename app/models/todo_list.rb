d4febd2530c8a1b15e7483ea65baf1d055c9c941
Kashyap Sodha <kashyap.sodha@shadowhealth.com>
2020-10-12 01:54:28 -0400

    add dummy tests

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,3 +1,7 @@
 class TodoList < ApplicationRecord
     has_many :todo_items
+
+    def my_name(name)
+        return true if name=='Kashyap'
+    end
 end
--- test/models/todo_list_test.rb
+++ test/models/todo_list_test.rb
@@ -4,4 +4,8 @@ class TodoListTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'my_name' do
+    t = TodoList.new
+    expect(t.name('Kashyap')).to be true
+  end
 end
